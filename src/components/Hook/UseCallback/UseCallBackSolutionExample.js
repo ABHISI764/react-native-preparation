44c466100f8aa901feb86a9ec761aa5de3aa3a92
Abhishek Pratap Singh <abhisi764@gmail.com>
2025-08-27 04:04:37 +0530

    call back example

--- src/components/Hook/UseCallback/UseCallBackSolutionExample.js
+++ src/components/Hook/UseCallback/UseCallBackSolutionExample.js
@@ -1,25 +1,27 @@
+
 import { View, Text, TouchableOpacity } from 'react-native';
 import React, { useCallback, useState } from 'react';
 import ChildButton from './ChildButton';
 
-const UseCallBackProblemExample = () => {
+const UseCallBackSolutionExample = () => {
   const [count, setCount] = useState(0);
   const [other, setOther] = useState(0);
 
-  const handelPress = () => {
+  const handelPress = useCallback(() => {
     setCount(count + 1);
-  };
+  },[]);
 
   return (
     <View style={{ padding: 20 }}>
       <Text>Count: {count}</Text>
-      <ChildButton onPress={handelPress} />
-      <TouchableOpacity onPress={() => setOther(other + 1)}>
+      <ChildButton  onPress={handelPress}/>
+      <TouchableOpacity onPress={()=> setOther(other +1)}>
         <Text>Update Other State</Text>
       </TouchableOpacity>
       <Text>SetOther: {other}</Text>
+
     </View>
   );
 };
 
-export default UseCallBackProblemExample;
+export default UseCallBackSolutionExample;
